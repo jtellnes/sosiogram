6429185dad168ab65be78f0863749b8c5b621f92
Jørgen Tellnes <jorgen@tellnes.nu>
2015-07-18 21:46:20 +0200

    Add master-view with bootstrap pulled in

--- resources/views/statements/create.blade.php
+++ resources/views/statements/create.blade.php
@@ -1,34 +1,39 @@
-<form method="POST" action="/statements">
-    {!! csrf_field() !!}
+@extends('master')
+@section('title', 'registrering')
 
-    <div>
-        <input type="hidden" name="survey" value="1">
-    </div>
+@section('content')
+    <form method="POST" action="/statements">
+        {!! csrf_field() !!}
 
-    <div class="col-md-6">
-        Gjeldende elev:
-        <select name="by_user">
-            @foreach($users as $user)
-                <option value="{{ $user->id }}">{{ $user->name }}</option>
-            @endforeach
-        </select>
-    </div>
+        <div>
+            <input type="hidden" name="survey" value="1">
+        </div>
 
-    <div class="col-md-6">
-        Situasjon
-        <input type="number" name="context">
-    </div>
+        <div class="col-md-6">
+            Gjeldende elev:
+            <select name="by_user" class="form-control">
+                @foreach($users as $user)
+                    <option value="{{ $user->id }}">{{ $user->name }}</option>
+                @endforeach
+            </select>
+        </div>
 
-    <div class="col-md-6">
-        Referert elev
-        <select name="ref_user">
-            @foreach($users as $user)
-                <option value="{{ $user->id }}">{{ $user->name }}</option>
-            @endforeach
-        </select>
-    </div>
+        <div class="col-md-6">
+            Situasjon
+            <input type="number" name="context">
+        </div>
 
-    <div>
-        <button type="submit">Svar</button>
-    </div>
-</form>
+        <div class="col-md-6">
+            Referert elev
+            <select name="ref_user" class="form-control">
+                @foreach($users as $user)
+                    <option value="{{ $user->id }}">{{ $user->name }}</option>
+                @endforeach
+            </select>
+        </div>
+
+        <div>
+            <button type="submit">Svar</button>
+        </div>
+    </form>
+@endsection
